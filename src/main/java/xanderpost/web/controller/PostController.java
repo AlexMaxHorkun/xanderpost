3b83524efa61fb1968bb0d164eddddbc30e81bce
alexander.horkun <alexander.horkun@x2sy.com>
2014-07-25 15:07:37 +0300

    blah

--- src/main/java/xanderpost/web/controller/PostController.java
+++ src/main/java/xanderpost/web/controller/PostController.java
@@ -1,16 +1,19 @@
 package xanderpost.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+import org.springframework.web.servlet.view.xml.MarshallingView;
 import xanderpost.entity.Post;
 import xanderpost.repository.PostDaoInterface;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
-@Controller
+@RestController
 public class PostController {
     private PostDaoInterface postDao;
 
@@ -22,12 +25,24 @@ public class PostController {
         this.postDao = postDao;
     }
 
-    @RequestMapping(value = {"/", "/posts"}, method = RequestMethod.GET)
-    public ModelAndView postsListAction(){
-        ModelAndView response=new ModelAndView();
+    @RequestMapping(value =  "/posts" , method = RequestMethod.GET)
+    public  ModelAndView postsListAction(@RequestHeader("Accept") List<String> acceptHeader){
         List<Post> posts=(ArrayList<Post>)postDao.findAll();
+        ModelAndView response=new ModelAndView();
         response.addObject("posts", posts);
-        response.setViewName("postsList");
+        View view=null;
+        if(acceptHeader.contains("application/json")){
+            MappingJackson2JsonView jsonView=new MappingJackson2JsonView();
+            jsonView.setModelKey("posts");
+            view=jsonView;
+            response.setViewName("jsonView");
+        }
+        else{
+            MarshallingView xmlView=new MarshallingView();
+            xmlView.setModelKey("posts");
+            xmlView.setBeanName("post");
+            view=xmlView;
+        }
         return response;
     }
 }
