df7c87069501507a235bbf1a143718b9d1184488
alexander.horkun <alexander.horkun@x2sy.com>
2014-07-30 12:19:16 +0300

    spring security

--- src/main/java/xanderpost/entity/User.java
+++ src/main/java/xanderpost/entity/User.java
@@ -1,6 +1,12 @@
 package xanderpost.entity;
 
-public class User {
+import org.springframework.security.core.userdetails.UserDetails;
+import xanderpost.security.GrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class User implements UserDetails{
     private long id;
 
     private String email;
@@ -29,5 +35,31 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Collection<GrantedAuthority> getAuthorities(){
+        Collection<GrantedAuthority> auths=new ArrayList<GrantedAuthority>();
+        auths.add(new GrantedAuthority());
+        return auths;
+    }
+
+    public boolean isCredentialsNonExpired(){
+        return true;
+    }
+
+    public String getUsername(){
+        return getEmail();
+    }
+
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+
+    public boolean isEnabled(){
+        return true;
     }
 }
--- src/main/java/xanderpost/security/UserProvider.java
+++ src/main/java/xanderpost/security/UserProvider.java
@@ -1,5 +1,22 @@
 package xanderpost.security;
 
-public class UserProvider{
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import xanderpost.entity.User;
+import xanderpost.repository.UserDaoInterface;
 
+public class UserProvider implements UserDetailsService{
+    private UserDaoInterface userDao;
+
+    public UserDaoInterface getUserDao() {
+        return userDao;
+    }
+
+    public void setUserDao(UserDaoInterface userDao) {
+        this.userDao = userDao;
+    }
+
+    public User loadUserByUsername(String n){
+        return getUserDao().findByEmail(n);
+    }
 }
