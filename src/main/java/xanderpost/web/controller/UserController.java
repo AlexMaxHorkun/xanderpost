6676871846b30198fa00f3efb5880252823246e3
alexander.horkun <alexander.horkun@x2sy.com>
2014-08-04 16:54:33 +0300

    spring security

---
+++ src/main/java/xanderpost/web/controller/UserController.java
@@ -0,0 +1,19 @@
+package xanderpost.web.controller;
+
+import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import xanderpost.entity.User;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @RequestMapping(value="/current", method= RequestMethod.GET, produces = {"application/json", "application/xml"})
+    public ModelAndView currentAction(@AuthenticationPrincipal User user){
+        ModelAndView response=new ModelAndView();
+        response.addObject("user",user);
+        return response;
+    }
+}
