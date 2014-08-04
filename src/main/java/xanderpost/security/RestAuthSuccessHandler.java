aa5be4c2b4a67b6fd2bb27b1f27f8268e946e932
alexander.horkun <alexander.horkun@x2sy.com>
2014-08-04 17:53:00 +0300

    security auth success handler

--- src/main/java/xanderpost/security/RestAuthSuccessHandler.java
+++ src/main/java/xanderpost/security/RestAuthSuccessHandler.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 
 public class RestAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     public void onAuthenticationSuccess(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response, org.springframework.security.core.Authentication authentication) throws java.io.IOException, javax.servlet.ServletException {
-        super.onAuthenticationSuccess(request, response, authentication);
+        //super.onAuthenticationSuccess(request, response, authentication);
         response.setStatus(HttpServletResponse.SC_OK);
     }
 }
