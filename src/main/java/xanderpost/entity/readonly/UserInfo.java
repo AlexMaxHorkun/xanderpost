e3299bc6dba77d86d1cd4ed45a691b84868bf60e
alexander.horkun <alexander.horkun@x2sy.com>
2014-08-18 13:44:45 +0300

    user readonly info

---
+++ src/main/java/xanderpost/entity/readonly/UserInfo.java
@@ -0,0 +1,51 @@
+package xanderpost.entity.readonly;
+
+import org.hibernate.annotations.Subselect;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Subselect("select u.id, u.email, count(p.id) as postsWritten, avg(pr.rate) as avgPostsWrittenRating from User u left join Post as p left join PostRating as pr group by u.id")
+public class UserInfo {
+    private long id;
+
+    private String email;
+
+    private int postsWritten;
+
+    private Float avgPostsWrittenRating;
+
+    @Id
+    public long getId() {
+        return id;
+    }
+
+    protected void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPostsWritten() {
+        return postsWritten;
+    }
+
+    public void setPostsWritten(int postsWritten) {
+        this.postsWritten = postsWritten;
+    }
+
+    public Float getAvgPostsWrittenRating() {
+        return avgPostsWrittenRating;
+    }
+
+    public void setAvgPostsWrittenRating(Float avgPostsWrittenRating) {
+        this.avgPostsWrittenRating = avgPostsWrittenRating;
+    }
+}
