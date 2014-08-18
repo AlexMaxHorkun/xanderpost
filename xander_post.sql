209f021563d7106ee3e74766e3d1306c1b410b41
alexander.horkun <alexander.horkun@x2sy.com>
2014-08-18 13:27:20 +0300

    schema

--- xander_post.sql
+++ xander_post.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Время создания: Авг 18 2014 г., 09:48
+-- Время создания: Авг 18 2014 г., 13:26
 -- Версия сервера: 5.5.38-0ubuntu0.14.04.1
 -- Версия PHP: 5.5.9-1ubuntu4.3
 
@@ -31,7 +31,8 @@ CREATE TABLE IF NOT EXISTS `Post` (
   `title` varchar(255) NOT NULL,
   `text` varchar(4000) NOT NULL,
   `author` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `author` (`author`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
 
 --
@@ -53,15 +54,16 @@ CREATE TABLE IF NOT EXISTS `PostRating` (
   `user_id` int(11) NOT NULL,
   `rate` smallint(6) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `user_id` (`user_id`,`post_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  UNIQUE KEY `user_id` (`user_id`,`post_id`),
+  KEY `post_id` (`post_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
 -- Дамп данных таблицы `PostRating`
 --
 
 INSERT INTO `PostRating` (`id`, `post_id`, `user_id`, `rate`) VALUES
-(1, 8, 8, 5);
+(2, 8, 12, 7);
 
 -- --------------------------------------------------------
 
@@ -76,7 +78,7 @@ CREATE TABLE IF NOT EXISTS `User` (
   `enabled` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
 
 --
 -- Дамп данных таблицы `User`
@@ -85,13 +87,7 @@ CREATE TABLE IF NOT EXISTS `User` (
 INSERT INTO `User` (`id`, `email`, `password`, `enabled`) VALUES
 (1, 'test@test.com', '$2a$04$zjHuPJ1YoPowVLFohaCjeOumqI7bb88//GBh4qDvwJO9Rt48TKL9i', 1),
 (2, 'admin@test.com', '$2a$04$GNOzn0dLRiGChAXCCz0fQekQ3AfeU3XavSSGoKtm8k9wLQJ.s6Vqq', 1),
-(4, 'test11@test.com', '$2a$10$c7BoCBAsTeARKwHaVJZaFeYadMKgQ.7TLYI7WsQLKKRGudfNBAH4O', 1),
-(5, 'test2_1@test.com', '$2a$10$9g73luvXfsl69YcnQm3EeunrDE8bu9kDhNpH2DANhJggv65U9hamG', 1),
-(6, 'test6@test', '$2a$10$5X72isSCiKy5cY5GyUDrWOGP6wdvElftEIBkgljbuiXSwZ10RwDzW', 1),
-(7, 'test7@test', '$2a$10$AGiEKeiiSfUEfJGMLEijrOApT4rLxC5zmxYWLVBYmXwd0dWWwNvje', 1),
-(8, 'test9@test.com', '$2a$10$tOjEKuRuEyx4pFm6yTmNduvqUs/HswFxCDqlFki3SR0p3jzZ1YjIu', 1),
-(9, 'test10@test.com', '$2a$10$9RvFz7QgEgkCw2mjBUtsHeJ6u7XUCZ1A9BCXLuNVZneDoZptuJPUu', 1),
-(11, 'testy12@test.com', '$2a$10$wxynUtKHO4uee1IrBnzItu7Lb96I4AO8vOWMi5j6g..bl64emiW0G', 1);
+(12, 'newtest@test.com', '$2a$10$5ffPvVsZ/dshxI/D98OPw.q.vzb6vK9Nf0Q/XlAkIv5kGbrTtXaDq', 1);
 
 -- --------------------------------------------------------
 
@@ -136,14 +132,24 @@ CREATE TABLE IF NOT EXISTS `user_role` (
 INSERT INTO `user_role` (`user_id`, `role_id`) VALUES
 (2, 2),
 (1, 3),
-(4, 3),
-(5, 3),
-(9, 3),
-(11, 3);
+(12, 3);
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
+
+--
+-- Ограничения внешнего ключа таблицы `Post`
+--
+ALTER TABLE `Post`
+  ADD CONSTRAINT `Post_ibfk_1` FOREIGN KEY (`author`) REFERENCES `User` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `PostRating`
+--
+ALTER TABLE `PostRating`
+  ADD CONSTRAINT `PostRating_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `Post` (`id`),
+  ADD CONSTRAINT `PostRating_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`);
 
 --
 -- Ограничения внешнего ключа таблицы `UserRole`
